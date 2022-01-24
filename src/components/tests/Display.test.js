c61cc76df3801bab4ac11263fc0d9f691c26d430
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2022-01-24 15:29:23 -0800

    module completed

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -3,13 +3,71 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+import mockFetchShow from '../../api/fetchShow';
+jest.mock('../../api/fetchShow')
+
 import Display from './../Display';
 
+const testShow ={
+    name: 'test show',
+    summary: 'test summary',
+    seasons: [
+        {
+            id: 0,
+            name: 'season 1',
+            episodes: []
+        },
+        {
+            id: 0,
+            name: 'season 2',
+            episodes: []
+        }
+    ]
+    
+}
 
 test('renders without errors with no props', ()=>{
+     render(<Display/>)
+});
+
+test('renders Show component when the button is clicked ', async () =>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
+
+    render(<Display/>)
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const show = await screen.findByTestId('show-container');
+    expect(show).toBeInTheDocument();
+});
+
+test('renders show season options matching your data when the button is clicked', async () =>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
+
+    render(<Display/>)
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    await waitFor(() => {
+        const seasonOptions = screen.queryAllByTestId('season-option');
+        expect(seasonOptions).toHaveLength(2);
+    })
 
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('display function is called when the fetch buttion is clicked', async () => {
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+    mockFetchShow.mockResolvedValueOnce(testShow);
+
+    const displayFunction = jest.fn();
+
+    render(<Display displayFunction={displayFunction}/>)
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    waitFor(() => {
+        expect(displayFunction).toHaveBeenCalled();
+    })
+
+
+})
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,40 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const testEpisode = {
+    id:1,
+    name:'',
+    image:'',
+    season:1,
+    number:1,
+    summary:'test summary',
+    runtime:1
+}
+    
+const episodeWithNoImage ={
+    id:1,
+    name:'',
+    image:null,
+    season:1,
+    number:1,
+    summary:'test summary',
+    runtime:1
+}
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    render(<Episode episode={testEpisode}/>)
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={testEpisode}/>)
+    const summary = screen.queryByText(/test summary/i);
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).not.toBeNull();
+});
 
-test("renders default image when image is not defined", ()=>{});
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={episodeWithNoImage}/>)
+    const image = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png')
+    expect(image).toBeInTheDocument();
+});
